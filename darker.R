678da1c68398470abb147df4ada2e9ba06f352c7
deeaetrader <64110633+deeaetrader@users.noreply.github.com>
2020-05-16 21:00:05 +0800

    Best 8 months of Nasdaq

---
+++ darker.R
@@ -0,0 +1,16 @@
+library(ggthemes)
+grim <- theme_fivethirtyeight() + theme(
+  panel.background = element_rect(fill="black"),
+  plot.background = element_rect(fill="black"),
+  legend.background = element_blank(),
+  legend.key = element_blank(),
+  legend.text = element_text(color = "white"),
+  plot.title = element_text(color = "white", face = "bold"),
+  axis.title = element_text(color = "white"),
+  axis.text = element_text(color = "white")
+)
+
+starbucks <- grim + theme(
+  panel.background = element_rect(fill="#006341", color = "black"),
+  plot.background = element_rect(fill="#006341", color = "black")
+)
